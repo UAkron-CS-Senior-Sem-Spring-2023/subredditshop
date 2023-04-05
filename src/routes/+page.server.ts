7a0ba9ea1f2ab5e1436125e86fb2e41f81a37796
Seth Milojevic <sm464@uakron.edu>
2023-04-05 14:46:10 -0400

    Create inital OpenAI call.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,20 @@
-import { env } from '../lib/env'
+import { ChatGPTAPI } from 'chatgpt'
+import { OPENAI_API_KEY } from '$env/static/private'
 
 export async function load() {
-    /*
+    if (OPENAI_API_KEY) {
+        const api = new ChatGPTAPI({
+            apiKey: OPENAI_API_KEY,
+            systemMessage: ''
+        });
+        const res = await api.sendMessage('Hello World!');
+        console.log(res.text);
+    } else {
+        console.log("ERR: No OpenAI Key");
+    }
+}
+/*
+export async function load() {
     let authResponse = await fetch('https://www.reddit.com/api/v1/access_token', {
         method: 'POST',
         headers: {
@@ -13,5 +26,5 @@ export async function load() {
     return {
         response: authResponseJSON,
     }
-    */
 }
+*/
